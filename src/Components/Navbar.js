90b407703bbff5a16dd398699db9da234407e83c
Ayush-13-02 <ayushsharma13jun@gmail.com>
2022-07-24 10:39:41 +0530

    Update Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import youImg from './Img/youtubeimg.png'
+import youImg from './Img/youtubelogo.png'
 import search from './Img/search.png'
 import mic from './Img/microphone.png'
 import bell from './Img/bell.png'
@@ -14,22 +14,21 @@ import Example from './drop'
 function Navbar({ open, signin, handleopen, handlelogin }) {
     return (
         <div className='h-16 m-0 p-0 w-full overflow-x-scroll scrollnav'>
-            <div className='h-16 min-w-[768px] flex justify-between flex-row bg-black bg-opacity-90'>
+            <div className='h-16 min-w-[790px] flex justify-between flex-row bg-black bg-opacity-90'>
                 <div className='flex justify-start ml-6 items-center'>
-                    <button className='mr-2 h-10 w-10 p-2 rounded-lg flex justify-center items-center hover:border' onClick={handleopen} >
+                    <button className='mr-2 h-10 w-10 p-1 rounded-lg flex justify-center items-center hover:border hover:rounded-full hover:border-gray-300/50 hover:shadow-lg' onClick={handleopen} >
                         <img src={Hambur} alt="..." className='h-6 w-6' />
                     </button>
                     <button className='flex items-center mx-1'>
-                        <img src={youImg} alt="..." className='h-6' />
-                        <div className='md:text-xl lg:text-2xl text-gray-500 font-bold flex'>YouTube <sup className='md:text-sm lg:text-base'>IN</sup> </div>
+                        <div className='flex'><img src={youImg} alt="..." className='max-h-20' /></div>
                     </button>
                 </div>
                 <div className='flex justify-center items-center sm:w-[40vw]'>
-                    <div className='h-10 flex'>
+                    <div className='h-10 flex w-[90%]'>
                         <input type="text" name="search" id="search" placeholder="Search" className=" text-xl px-2 py-2 border border-gray-500 text-white  bg-black bg-opacity-90 w-full" />
                     </div>
-                    <button className='justify-center border border-gray-500 h-10'><img src={search} alt="" className='p-1 h-10' /></button>
-                    <button className='justify-center flex'><img src={mic} alt="" className='h-8' /></button>
+                    <button className='flex items-center justify-center border border-gray-500 h-10 w-20'><img src={search} alt="" className='p-1 h-10' /></button>
+                    <button className='justify-center flex m-1'><img src={mic} alt="" className='h-8' /></button>
                 </div>
                 <div className='flex justify-end mr-4 items-center'>
                     <button className='justify-center mx-2' style={signin ? null : { display: "none" }}><img src={addvedio} alt="" className='h-10 w-10' /></button>
@@ -43,7 +42,7 @@ function Navbar({ open, signin, handleopen, handlelogin }) {
 
                     {signin ? <button className='mx-2 h-8 w-8 rounded-full overflow-hidden'><img src={pic} alt="" className='h-8 w-8'/></button> :
                         <button className='mx-2 justify-center border border-blue-600 h-10 flex items-center p-2'
-                            onClick={handlelogin}><img src={user} alt="" className='h-6' /><span className='text-xl text-blue-600'>Sign in</span></button>}
+                            onClick={handlelogin}><img src={user} alt="" className='h-6' /><span className='text-xl text-blue-600 p-1'>Sign in</span></button>}
                 </div>
 
             </div>
--- src/Components/Vedio.js
+++ src/Components/Vedio.js
@@ -12,19 +12,19 @@ function Vedio({ login }) {
         <>
             <div className='w-full h-full' style={login ? { display: "none" } : null}>
                 <hr />
-                <div className='h-full w-full overflow-y-scroll scrollbar'>
+                <div className='h-full w-full overflow-y-scroll scrollnav md:scrollbar'>
                     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 place-items-center'>
                         {
                             list.map((item, i) => {
                                 return <div key={i} className="max-w-sm rounded overflow-hidden shadow-lg m-2">
                                     <a href='https://www.youtube.com/watch?v=vo943l4Pp0I'>
                                         <img className="w-full" src={item.image} alt="Sunset in the mountains" />
-                                        <div className="px-1 overflow-y-hidden py-2 flex">
-                                            <img className='h-12 w-12 rounded-full mr-2' src={build} alt='' />
-                                            <div className="font-medium text-base p-1 mb-2 text-white">Dirty Little Secret - Nora Fatehi x Zack Knight (EXCLUSIVE Music Video)</div>
+                                        <div className="p-1 overflow-y-hidden flex items-center justify-center">
+                                            <img className='h-10 w-10 rounded-full mr-2' src={build} alt='' />
+                                            <div className="font-semibold w-full max-h-12 overflow-hidden whitespace-normal text-ellipsis text-[0.925rem] p-1 mb-2 text-white">Dirty Little Secret - Nora Fatehi x Zack Knight (EXCLUSIVE Music Video)</div>
 
                                         </div>
-                                        <div className='text-gray-100 text-sm font-normal m-1 px-4 ml-12'>
+                                        <div className='text-gray-400 text-sm px-4 ml-12'>
                                             <h6>Channel Name</h6>
                                             <span>100k veiws </span>
                                             <span className='mx-1 mb-1'>.</span>
